b82480bcc4b43063b797a97d5cfb5b986980c70d
Nina Valenzuela <98240042+sharkbaitnina@users.noreply.github.com>
2022-10-05 08:57:54 -0700

    Create smallest-integer-solution.js

---
+++ smallest-integer-solution.js
@@ -0,0 +1,8 @@
+//Given the first line of code
+Class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(…args)
+  }
+}
+//use Math.min to find lowest number in array
+// args is given in code to represent the array of numbers
